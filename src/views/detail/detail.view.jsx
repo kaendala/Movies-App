7fe5610e4067c65b135edc1c3263d36a18677ae2
kaendala <62041789+kaendala@users.noreply.github.com>
2021-07-15 17:16:08 -0500

    detail was created

--- src/views/detail/detail.view.jsx
+++ src/views/detail/detail.view.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Const from "../../const/general.const.json";
 
-const Detail = ({ id }) => {
-  return <div className="errorPageContainer">Detail</div>;
+const Detail = ({ match: { params } }) => {
+  return <CurrentDetail movieId={params.movieId}></CurrentDetail>;
+};
+const CurrentDetail = ({ movieId }) => {
+  const [movie, setMovie] = useState(null);
+  useEffect(() => {
+    if (movieId) {
+      const url = `${Const.api_url}movie/${movieId}?api_key=${Const.api_key}`;
+      axios.get(url).then((res) => {
+        setMovie(res.data);
+      });
+    }
+  }, [movieId]);
+  return (
+    <>
+      {movie && (
+        <div className="detail">
+          <div>
+            <h4 className="title">{movie.title}</h4>
+          </div>
+          <div className="contentImage">
+            <img src={`${Const.api_image}${movie["backdrop_path"]}`}></img>
+          </div>
+          <div className="description">
+            <h4>Original title: {movie["original_title"]}</h4>
+            <h4>Popularity: {movie["popularity"]}</h4>
+            <h4>Vote average: {movie["vote_average"]}</h4>
+            <h4>Vote count: {movie["vote_count"]}</h4>
+          </div>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default Detail;
